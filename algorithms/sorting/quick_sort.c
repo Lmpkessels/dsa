ca7d9e5ac88d4d388434b6d683791633cadf2aaa
lkessels <l@lmpkessels.com>
2026-06-25 11:36:28 +0200

    add(algorithm): quick sort to sort an array

---
+++ algorithms/sorting/quick_sort.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+void swap(int *a, int *b);
+
+/* Partition, gets the index of where pivot needs to be
+placed */
+int partition(int arr[], int low, int high)
+{
+    int pivot, i, j;
+
+    pivot = arr[high];
+    i = low - 1;
+
+    /* Get the index for the partition and move indices
+    less then the pivot to the left side */
+    for (j = low; arr[j] <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+
+    // Pivot is placed on the correct index
+    swap(&arr[i + 1], &arr[high]);
+    return i + 1;
+}
+
+/* Sort the array trough recursion which works like a 
+kind of loop that updates the state of the data-structure 
+at every itteration */
+void quick_sort(int arr[], int low, int high)
+{
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quick_sort(arr, low, pi - 1);
+        quick_sort(arr, pi + 1, high);
+    }
+}
+
+// Swap a with b
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Prints the sorted array
+void print_sorted_arr(int arr[], int n)
+{
+    int i;
+
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+}
+
+int main(void)
+{
+    int arr[6] = {10, 5, 15, 12, 3, 0};
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    quick_sort(arr, 0, n - 1);
+
+    print_sorted_arr(arr, n);
+}
