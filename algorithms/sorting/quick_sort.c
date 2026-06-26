61938e80893bbbb96fbfb342a511c1f60d595d48
lkessels <l@lmpkessels.com>
2026-06-26 23:45:01 +0200

    remove: function to print array

--- algorithms/sorting/quick_sort.c
+++ algorithms/sorting/quick_sort.c
@@ -46,15 +46,3 @@ void swap(int *a, int *b)
     *a = *b;
     *b = temp;
 }
-
-// Prints the sorted array
-void print_sorted_arr(int arr[], int n)
-{
-    int i;
-
-    for (i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-
-    printf("\n");
-}
